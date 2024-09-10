1ff1d302907889f77718302bc1c5bbff7d302c33
endomorphosis <starworks5@gmail.com>
2024-09-10 05:26:06 -0700

    Refactor code to handle file paths correctly in ipfs_multiformats.py and ipfs_only_hash.py

--- ipfs_embeddings_py/ipfs_only_hash.py
+++ ipfs_embeddings_py/ipfs_only_hash.py
@@ -1,15 +1,22 @@
 import os
 import subprocess
-
+import tempfile
 class ipfs_only_hash_py:
     def __init__(self, resources, metadata):
 
         return None
     
-    def __call__(self, file_path):
-        absolute_path = os.path.abspath(file_path)
-        ipfs_hash_cmd = "bash -c 'npx ipfs-only-hash " + absolute_path 
-        ipfs_hash = subprocess.check_output(ipfs_hash_cmd, shell=True).decode('utf-8').strip()
+    def __call__(self, request):
+        if os.path.isfile(request) == True:
+            absolute_path = os.path.abspath(request)
+            ipfs_hash_cmd = "bash -c 'npx ipfs-only-hash " + absolute_path 
+            ipfs_hash = subprocess.check_output(ipfs_hash_cmd, shell=True).decode('utf-8').strip()
+        else:
+            with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+                f.write(request)
+                filename = f.name
+                ipfs_hash_cmd = "bash -c 'npx ipfs-only-hash " + filename + "'"
+                ipfs_hash = subprocess.check_output(ipfs_hash_cmd, shell=True).decode('utf-8').strip()
         return ipfs_hash
     
     def __test__(self):
