7ecbffa0353692e44d13bf5039067f5b19bb8a92
Benjamin Barber <starworks5@gmail.com>
2024-12-19 21:16:08 -0800

    Bump version to 0.0.28, update setup.py version, and include additional node parser packages

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 setup(
     name="ipfs_embeddings_py",
-	version='0.0.24',
+	version='0.0.27',
 	packages=[
 		'ipfs_embeddings_py',
         'ipfs_embeddings_py.huggingface',
         'ipfs_embeddings_py.node_parser',
+        'ipfs_embeddings_py.node_parser.file',
+        'ipfs_embeddings_py.node_parser.text',
+        'ipfs_embeddings_py.node_parser.relational',        
 	],
 	install_requires=[
         'transformers',
